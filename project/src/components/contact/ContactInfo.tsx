224799adfaddf763be7df3c51d05e2a046f6b1ce
muddappamk-spec <muddappa.mk@gmail.com>
2025-07-23 22:03:22 +0530

    new

--- project/src/components/contact/ContactInfo.tsx
+++ project/src/components/contact/ContactInfo.tsx
@@ -35,15 +35,7 @@ const ContactInfo: React.FC = () => {
       
       <div className="mt-8">
         <div className="rounded-lg overflow-hidden h-64">
-          <iframe 
-            src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8&q=${encodeURIComponent(companyInfo.address)}`}
-            width="100%" 
-            height="100%" 
-            style={{ border: 0 }} 
-            allowFullScreen 
-            loading="lazy"
-            title="Company Location"
-          ></iframe>
+          
         </div>
       </div>
     </div>
